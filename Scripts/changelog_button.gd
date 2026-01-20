ad32ef0d3b3e0f3bf68fdaea0dce8cf34db92ac7
amathew4538 <amathew4538@colliervilledragons.org>
2026-01-19 21:20:20 -0600

    V0.10a Bugfix for Changelog Button

--- Scripts/changelog_button.gd
+++ Scripts/changelog_button.gd
@@ -1,15 +1,19 @@
 extends Control
 
 @export var changelog_scene: PackedScene 
+var active_changelog: Node = null  # Store the reference here
 
 func _on_pressed() -> void:
+	# If a changelog already exists and hasn't been deleted, stop here
+	if is_instance_valid(active_changelog):
+		return
+		
 	var instance = changelog_scene.instantiate()
+	active_changelog = instance # Save the reference
 	add_child(instance)
 	
-	# Get the current time position of the main menu's animation
 	var current_time = $"../AnimationPlayer".current_animation_position
-	
-	# Tell the changelog animation to start at that exact same time
 	var instance_anim = instance.get_node("AnimationPlayer")
+	
 	instance_anim.play("changelog_move")
 	instance_anim.seek(current_time, true)
